731b47af262616bbf360766c3d734db72365c032
junkiemonke <156279771+junkiemonke@users.noreply.github.com>
2024-02-13 10:58:50 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
-
-public class calculator {
+public class Calculator {
     public static void main(String[] args) {
-
         int a=20;
+        System.out.println(" the first number is:"+a);
         int b=10;
+        System.out.println(" the second number is:"+b);
         addition(a, b);
         subtraction(a, b);
         division(a, b);
@@ -14,25 +13,28 @@ public class calculator {
     }
 
     public static void addition(int a, int b) {
-        System.out.println("Addition:"+(a+b));
+        System.out.println("Reasult of Addition is:"+(a+b));
     }
 
     public static void subtraction(int a, int b) {
-        System.out.println("Substraction:"+(a-b));
+        System.out.println("Reasult of Substraction is:"+(a-b));
     }
 
     public static void division(int a, int b) {
-        System.out.println("Division:"+(a/b));
+        if(b == 0) {
+            System.out.println("Division: Division by zero is not allowed.");
+        } else {
+            System.out.println("Reasult of Division is:"+(a/b));
+        }
     }
 
     public static void multiplication(int a, int b) {
-        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Reasult of Multiplication is:"+(a*b));
     }
-
-    public static void  square(int a){
-            System.out.println("Square:"+(a*a));
+    public static void square(int a){
+        System.out.println("Result of Square is: "+ (a * a));
     }
-    public static void  cube(int a){
-                System.out.println("Cube:"+(a*a*a));
+    public static void cube(int a){
+        System.out.println("Result of Cube is: "+ (a * a * a));
     }
 }
